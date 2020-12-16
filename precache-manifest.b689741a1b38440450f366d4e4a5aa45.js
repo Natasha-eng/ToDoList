042d8efe97863314437f42df8df591123116ea54
Natasha-eng <natasha.shilova.89@gmail.com>
2020-12-16 13:35:40 +0300

    Updates

---
+++ precache-manifest.b689741a1b38440450f366d4e4a5aa45.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17e506cc898791b4c6fba715c46a031b",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "20efc64c6c382a062e3d",
+    "url": "/ToDoList/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "5e779883137d63b1bc14",
+    "url": "/ToDoList/static/js/2.22d19bde.chunk.js"
+  },
+  {
+    "revision": "d2266803016540034565984c8bb59069",
+    "url": "/ToDoList/static/js/2.22d19bde.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20efc64c6c382a062e3d",
+    "url": "/ToDoList/static/js/main.da79d410.chunk.js"
+  },
+  {
+    "revision": "448f1ac8991118963dc3",
+    "url": "/ToDoList/static/js/runtime-main.97949b04.js"
+  }
+]);
